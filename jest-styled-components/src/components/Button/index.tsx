cdcf6194362fdeb4dc97200e0ffdd7c863fb6745
Caio Barilli <caiobarilli@gmail.com>
2023-03-22 19:46:48 -0300

    Update index.tsx

--- jest-styled-components/src/components/Button/index.tsx
+++ jest-styled-components/src/components/Button/index.tsx
@@ -1,45 +1,75 @@
-import React, {
-  forwardRef,
-  AnchorHTMLAttributes,
-  ButtonHTMLAttributes
-} from 'react'
+import styled, { css, DefaultTheme } from 'styled-components'
 
-import * as S from './styles'
+import { ButtonProps } from '.'
 
-type ButtonTypes =
-  | AnchorHTMLAttributes<HTMLAnchorElement>
-  | ButtonHTMLAttributes<HTMLButtonElement>
+export type WrapperProps = { hasIcon: boolean } & Pick<
+  ButtonProps,
+  'size' | 'fullWidth' | 'minimal'
+>
 
-export type ButtonProps = {
-  size?: 'small' | 'medium' | 'large'
-  fullWidth?: boolean
-  minimal?: boolean
-  icon?: JSX.Element
-  as?: React.ElementType
-} & ButtonTypes
+const WrapperModifiers = {
+  small: (theme: DefaultTheme) => css`
+    height: 3rem;
+    font-size: ${theme.font.sizes.xsmall};
+  `,
 
-const Button: React.ForwardRefRenderFunction<S.WrapperProps, ButtonProps> = (
-  {
-    children,
-    icon,
-    size = 'medium',
-    fullWidth = false,
-    minimal = false,
-    ...props
-  },
-  ref
-) => (
-  <S.Wrapper
-    size={size}
-    fullWidth={fullWidth}
-    minimal={!!minimal}
-    hasIcon={!!icon}
-    ref={ref}
-    {...props}
-  >
-    {!!icon && icon}
-    {!!children && <span>{children}</span>}
-  </S.Wrapper>
-)
+  medium: (theme: DefaultTheme) => css`
+    height: 4rem;
+    font-size: ${theme.font.sizes.medium};
+  `,
 
-export default forwardRef(Button)
+  large: (theme: DefaultTheme) => css`
+    height: 4.35rem;
+    font-size: ${theme.font.sizes.medium};
+  `,
+
+  withIcons: (theme: DefaultTheme) => css`
+    svg {
+      width: 1.5rem;
+
+      & + span {
+        margin-left: ${theme.spacings.xxsmall};
+      }
+    }
+  `,
+
+  fullWidth: () => css`
+    width: 100%;
+  `,
+
+  minimal: (theme: DefaultTheme) => css`
+    background: transparent;
+    color: ${theme.colors.defaultText};
+
+    &:hover {
+      background: transparent;
+    }
+  `,
+
+  disabled: () => css`
+    &:disabled {
+      cursor: not-allowed;
+      filter: saturate(30%);
+    }
+  `
+}
+
+export const Wrapper = styled.button<WrapperProps>`
+  ${({ theme, size, fullWidth, hasIcon, minimal, disabled }) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    text-decoration: none;
+    cursor: pointer;
+
+    font-family: ${theme.font.family};
+    border-radius: ${theme.border.radius};
+
+    ${!!size && WrapperModifiers[size](theme)}
+    ${!!fullWidth && WrapperModifiers.fullWidth()}
+    ${!!hasIcon && WrapperModifiers.withIcons(theme)}
+    ${!!minimal && WrapperModifiers.minimal(theme)}
+    ${disabled && WrapperModifiers.disabled()};
+  `}
+`
