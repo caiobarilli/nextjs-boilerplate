1d96f1236cbec2258c2f415f56055e58bdd3c02f
Caio Barilli <caiobarilli@gmail.com>
2024-04-24 20:03:41 -0300

    add app_router boilerplate

---
+++ app_router/jest-eslint-prettier/README.md
@@ -0,0 +1,11 @@
+## Next.js Boilerplate
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
---
+++ app_router/jest-eslint-prettier/src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <div>
+      <h1>NextJs</h1>
+    </div>
+  )
+}
+
+export default Main
